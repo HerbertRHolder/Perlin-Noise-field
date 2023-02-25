577d13ed369ec9213badd5b5d10d5809a20c1d41
Herb <herbrholder@gmail.com>
2023-02-25 14:22:35 -0600

    Add: particles

--- particles.js
+++ particles.js
@@ -1,11 +1,14 @@
 let particles = [];
 const num = 10000;
-
-const noiseScale = 0.01/2;
+const noiseScale = 0.01 / 2;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  for(let i = 0; i < num; i ++) {
+  let parent = document.getElementById("canvas");
+  let canvas = createCanvas(1000, 800);
+  canvas.parent(parent)
+  // console.log(canvas);
+  // canvas.after(hero);
+  for (let i = 0; i < num; i++) {
     particles.push(createVector(random(width), random(height)));
   }
   
@@ -18,54 +21,51 @@ function setup() {
 
 function draw() {
   background(0, 10);
-  for(let i = 0; i < num; i ++) {
+  for (let i = 0; i < num; i++) {
     let p = particles[i];
     point(p.x, p.y);
-    let n = noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale * noiseScale);
+    let n = noise(
+      p.x * noiseScale,
+      p.y * noiseScale,
+      frameCount * noiseScale * noiseScale
+    );
     let a = TAU * n;
     p.x += cos(a);
     p.y += sin(a);
-    if(!onScreen(p) && frameCount < 100000) {
+    if (!onScreen(p) && frameCount < 100000) {
       p.x = random(width);
       p.y = random(height);
     }
   }
 }
 
-// function mouseReleased() {
-//   noiseSeed(millis());
-// }
+function mouseReleased() {
+  noiseSeed(millis());
+}
 
 function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
 }
-
 
 // const reloadUsingLocationHash = () => {
 //   window.location.hash = "reload";
 // }
 // window.onload = reloadUsingLocationHash();
 
-
- // JavaScript anonymous function
- (() => {
+// JavaScript anonymous function
+(() => {
   if (window.localStorage) {
-
-      // If there is no item as 'reload'
-      // in localstorage then create one &
-      // reload the page
-      if (!localStorage.getItem('reload')) {
-          localStorage['reload'] = true;
-          window.location.reload();
-      } else {
-
-          // If there exists a 'reload' item
-          // then clear the 'reload' item in
-          // local storage
-          localStorage.removeItem('reload');
-      }
+    // If there is no item as 'reload'
+    // in localstorage then create one &
+    // reload the page
+    if (!localStorage.getItem("reload")) {
+      localStorage["reload"] = true;
+      window.location.reload();
+    } else {
+      // If there exists a 'reload' item
+      // then clear the 'reload' item in
+      // local storage
+      localStorage.removeItem("reload");
+    }
   }
 })(); // Calling anonymous function here only
-
-
-
